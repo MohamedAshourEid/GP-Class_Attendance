c3b616d62a42e2aa523a770fd126377b1445911b
eman ayman <emanayman657@yahoo.com>
2021-05-21 18:26:30 +0200

    kmeans (quiz)

--- app/Http/Controllers/K_Means/KmeansController.php
+++ app/Http/Controllers/K_Means/KmeansController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\K_Means;
 //require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Models\question;
+use phpDocumentor\Reflection\Type;
 use Phpml\Classification\KNearestNeighbors;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
@@ -14,28 +16,71 @@ class KmeansController extends Controller
 {
     public static function readData()
     {
-        $studentIDs=Student::query()->select('id')
+        $studentIDs=Student::query()->select('student_id')
             ->get();
-
         $gradesData=Grade::query()->select('student_id','grade')
             ->get();
-//        return $gradesData;
-        $studentsWithGrades=array();
+        $studentsWithGrades=[];
         $j=0;
         foreach ($studentIDs as $id)
         {
+            $gradesStudent=array();
+            //$gradesStudent['id']=$id->student_id;
+            //echo $id->student_id."----";
+            //$i=1;
             foreach ($gradesData as $grade)
             {
-                if($grade->student_id==$id->id)
+                if($grade->student_id==$id->student_id)
                 {
-                    $studentsWithGrades[$j++] = [$grade->grade,$id->id];
+                    $gradesStudent[]=(float)$grade->grade;
                 }
             }
+            $studentsWithGrades[$id->student_id]=$gradesStudent;
         }
-        $samples = [[1, 1,5], [8, 7,9], [1, 2,6], [7, 8,7], [2, 1,0], [8, 9,5]];
-        $kmeans = new KMeans(3);
-//        return $studentsWithGrades;
-        return $kmeans->cluster($studentsWithGrades);
+        $clusterer = new KMeans(5);
+        $clusters = $clusterer->cluster($studentsWithGrades);
+        $performance = array();
+
+        for ($i = 0; $i < 5; $i++){
+        $numberofstudents= count(array_keys($clusters[$i]));
+        $numberofquizzes=count(array_values($clusters[$i])[0]);
+        $final=0;
+            for ($j = 0; $j < $numberofstudents; $j++)
+            {
+
+                $sumofgrades = array_sum(array_values($clusters[$i])[$j]);
+                $averageofgrades=$sumofgrades/$numberofquizzes;
+
+                $final+=$averageofgrades;
+
+            }
+            $rate="";
+            if (($final/$numberofstudents)*10>=85){
+                $rate="A";
+
+            }
+            elseif (($final/$numberofstudents)*10>=75){
+                $rate="B";
+
+            }
+            elseif (($final/$numberofstudents)*10>=65){
+                $rate="C";
+
+            }
+            elseif (($final/$numberofstudents)*10>=50){
+                $rate="D";
+
+            }
+            else{
+                $rate="F";
+
+            }
+
+
+            $performance ["cluster ".($i+1)]=$rate;
+
+    }
+        print_r($performance);
 
     }
 }
